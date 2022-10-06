b9079ec01e515c750daf007c0bed273d3008c940
Duyen Nguyen <hongduyen0705@gmail.com>
2022-10-06 10:09:25 +0300

    wrote a few tests, changed blogSchema to set default value of like to 0 if its missing

--- blog-backend/tests/blog_api.test.js
+++ blog-backend/tests/blog_api.test.js
@@ -26,3 +26,31 @@ test("returns a correct amount of blog posts", async () => {
 
   expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
+
+test("4.9 blog post has id property", async () => {
+  const response = await api.get("/api/blogs");
+  const notes = response.body;
+  for (let note of notes) {
+    expect(note.id).toBeDefined();
+  }
+});
+
+test("4.10 HTTP POST request to /api/blogs url creates a new blog", async () => {
+  const newBlog = {
+    title: "post a blog with async await",
+    author: "fullstackopen",
+    url: "fullstackopen.com",
+    likes: 0,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  const titles = blogsAtEnd.map((b) => b.title);
+  expect(titles).toContain("post a blog with async await");
+});
--- blog-backend/tests/test_helper.js
+++ blog-backend/tests/test_helper.js
@@ -6,19 +6,20 @@ const initialBlogs = [
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
+    id: 1,
   },
   {
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
+    id: 2,
   },
 ];
 
 const blogsInDb = async () => {
   const blogs = Blog.find({});
-  return (await blogs).map((blog) => {
-    console.log("blog", blog);
+  return await blogs.map((blog) => {
     return blog.toJSON();
   });
 };
