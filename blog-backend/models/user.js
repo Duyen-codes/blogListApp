cc635d07ac247e686f3af0461ee8db57383b5acf
Duyen Nguyen <hongduyen0705@gmail.com>
2022-10-14 16:27:37 +0300

    defined model for users, mongoose schema for users

---
+++ blog-backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema =  mongoose.Schema({
+    username: String,
+    name: String,
+    passwordHash: String
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
