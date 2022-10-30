992e782cb28e70dee5c834669b6273082a6fc4c4
Duyen-codes <hongduyen0705@gmail.com>
2022-10-30 11:59:51 +0200

    added new custom command for making a new blog

--- bloglist-frontend/cypress/support/commands.js
+++ bloglist-frontend/cypress/support/commands.js
@@ -33,3 +33,18 @@ Cypress.Commands.add("login", ({ username, password }) => {
     cy.visit("http://localhost:3000");
   });
 });
+
+Cypress.Commands.add("createBlog", ({ title, author, url, likes }) => {
+  cy.request({
+    url: "http://localhost:3003/api/blogs",
+    method: "POST",
+    body: { title, author, url, likes },
+    headers: {
+      Authorization: `bearer ${
+        JSON.parse(localStorage.getItem("loggedBlogAppUser")).token
+      }`,
+    },
+  });
+
+  cy.visit("http://localhost:3000");
+});
