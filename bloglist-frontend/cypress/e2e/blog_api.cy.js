fb27f86d460cdf34e23299ddde1803c7058710de
Duyen-codes <hongduyen0705@gmail.com>
2022-10-31 15:49:09 +0200

    wrote last e2e test

--- bloglist-frontend/cypress/e2e/blog_api.cy.js
+++ bloglist-frontend/cypress/e2e/blog_api.cy.js
@@ -6,6 +6,8 @@ describe("Blog app", function () {
       username: "mluukkai",
       password: "salainen",
     };
+
+    const secondUser = {};
     cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
